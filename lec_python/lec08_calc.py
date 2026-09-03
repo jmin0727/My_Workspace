a6809cd0a739c32e68faf5af32b97f1a97f670c5
leejeongmin <you@example.com>
2026-09-03 17:58:35 +0900

    dddddd

---
+++ lec_python/lec08_calc.py
@@ -0,0 +1,14 @@
+#계산기 class 만들기
+
+class CalcClass :
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def sub(self, num1, num2):
+        return num1 - num2
+
+    def mul(self, num1, num2):
+        return num1 * num2
+
+    def div(self, num1, num2):
+        return num1 / num2
